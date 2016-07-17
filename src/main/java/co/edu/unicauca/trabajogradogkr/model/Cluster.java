af14a5d727bb651ad86a214d5480e2a294c7e7c4
equipo <equipo@equipo>
2016-07-16 20:44:59 -0500

    Cluster.

---
+++ src/main/java/co/edu/unicauca/trabajogradogkr/model/Cluster.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2016 Arnold Jair Jimenez Vargas <ajjimenez@unicauca.edu.co>.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package co.edu.unicauca.trabajogradogkr.model;
+
+/**
+ *
+ * @author Arnold Jair Jimenez Vargas <ajjimenez@unicauca.edu.co>
+ */
+public class Cluster {
+
+    private Record centroid;
+    private Record[] records;
+
+    public Record getCentroid() {
+        return centroid;
+    }
+
+    public void setCentroid(Record centroid) {
+        this.centroid = centroid;
+    }
+
+    public Record[] getRecords() {
+        return records;
+    }
+
+    public void setRecords(Record[] records) {
+        this.records = records;
+    }
+
+}
